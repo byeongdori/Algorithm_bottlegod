93841541d5b78df96d0a6d4be639a2ef97adda92
superbottlegod <superbottlegod@naver.com>
2021-07-14 16:34:59 +0900

    7/14

---
+++ Backjoon/2839.py
@@ -0,0 +1,10 @@
+a = int(input())
+temp = 9999
+for i in range(0, 1001):
+    for j in range(0, 1700):
+        if 5 * i + 3 * j == a and i + j < temp:
+            temp = i + j
+
+if temp == 9999:
+    temp = -1
+print(temp)
