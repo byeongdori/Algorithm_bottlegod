5cc87baaafa396caa9d2805ace9c7030d01166f2
superbottlegod <superbottlegod@naver.com>
2021-07-31 00:45:55 +0900

    Clear

--- BAEKJOON/Not Solved Yet/1929.cpp
+++ BAEKJOON/Not Solved Yet/1929.cpp
@@ -1,5 +1,6 @@
 // 1929 - 소수 구하기
-// 에라토스테네스 체 사용해서 다시 풀기
+// 에라토스테네스의 체 사용해서 다시 풀기
+
 #include <iostream>
 #include <math.h>
 using namespace std;
@@ -7,34 +8,31 @@ using namespace std;
 int main() {
 
     int x, y;
-    int sqrt_x;
     cin >> x >> y;
+
+    bool check_arr[1000001];
+
+    for (int i = 1; i < 1000001; i++)
+        check_arr[i] = true;
+
+    check_arr[1] = false;
 
     // 제곱근 까지만 소수 판별
 
-    for (x; x <= y; x++)
+    int root_y = int(sqrt(y));
+
+    for (int i = 2; i <= root_y; i++)
     {
-        if (x == 1)
-            continue;
-
-        if (x == 2 || x == 3)
+        if (check_arr[i] == true)
         {
-            cout << x << endl;
-            continue;
+            for (int j = 2; i * j <= 1000001; j++)
+                check_arr[i * j] = false;
         }
+    }
 
-        if (x % 2 == 0 || x % 3 == 0)
-            continue;
-
-        sqrt_x = int(sqrt(x));
-        for (int z = 2; z <= sqrt_x; z++)
-        {
-            cout << "double roop" << x << endl;
-
-            if (x % z == 0 && z != 1) 
-                break;
-            if (z == sqrt_x)
-                cout << x << endl;
-        }
+    for (int i = x; i <= y; i++)
+    {
+        if (check_arr[i] == true)
+            cout << i << endl;
     }
 }
