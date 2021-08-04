2105023b36f37b804cd3f280404cede57b30b930
superbottlegod <superbottlegod@naver.com>
2021-08-05 01:06:08 +0900

    8/5

--- BAEKJOON/Not Solved Yet/8958.cpp
+++ BAEKJOON/Not Solved Yet/8958.cpp
@@ -1,2 +1,35 @@
 // 8958 - O/X 퀴즈
+// 문자열 비교 다시하기!
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main() {
+
+    int total_score, score, test_case;
+
+    cin >> test_case;
+    cin.get();
+
+    char inputOX[80];
+
+    for (int i = 0; i < test_case; i++) {
+        cin.getline(inputOX, 81);
+        total_score = 0;
+        score = 1;
+        int j = 0;
+        while (inputOX[j] == 'O' || inputOX[j] == 'X') {
+            if (inputOX[j] == '0') {
+                cout << "hello";
+                if (j != 0 && inputOX[j - 1] != '0')
+                    score = 1;
+                total_score += score;
+            }
+            else
+                score = 1;
+            j += 1;
+        }
+        cout << total_score << endl;
+    }
+}
