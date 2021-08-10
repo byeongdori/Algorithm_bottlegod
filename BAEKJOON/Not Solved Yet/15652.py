620723c6411baa40bbf27846092e8e36a18f2134
superbottlegod <superbottlegod@naver.com>
2021-08-11 01:28:12 +0900

    8/10

--- BAEKJOON/Not Solved Yet/15652.py
+++ BAEKJOON/Not Solved Yet/15652.py
@@ -1,1 +1,15 @@
 # 15652 - N과 M (4)
+# 배열 사용해서 마무리하기
+input_split = input().split()
+
+n = int(input_split[0])
+m = int(input_split[1])
+
+current_min_num = 1
+
+output_list = []
+for i in range(0, m):
+    output_list.append(1)
+
+
+print(n, m)
