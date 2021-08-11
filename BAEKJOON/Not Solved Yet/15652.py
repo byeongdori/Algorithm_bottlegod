a45f8be8074ecfb56b9b43862e23559f004df681
superbottlegod <superbottlegod@naver.com>
2021-08-12 02:13:02 +0900

    8/11

--- BAEKJOON/Not Solved Yet/15652.py
+++ BAEKJOON/Not Solved Yet/15652.py
@@ -1,15 +1,24 @@
 # 15652 - N과 M (4)
-# 배열 사용해서 마무리하기
+# 1~N개까지의 자연수 중에서 M개 선택하여 출력 / 비내림차순 / 중복허용
+# 백트래킹
+
 input_split = input().split()
 
 n = int(input_split[0])
 m = int(input_split[1])
 
-current_min_num = 1
+output = []
 
-output_list = []
-for i in range(0, m):
-    output_list.append(1)
+def back_tracking(n, m, current, loop):
 
+    # 재귀 종료 조건
+    if loop == m:
+        print(*output)
 
-print(n, m)
+    # 재귀 내부
+    for i in range (current, n):
+        output.append(i)
+        back_tracking(n, m, current, loop + 1)
+        output.pop()
+
+back_tracking(n, m, 1, 1)
