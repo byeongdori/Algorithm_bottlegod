630509b2a714bf26263c1278d72787fca2e92b4b
superbottlegod <superbottlegod@naver.com>
2021-09-01 15:09:04 +0900

    9/1

--- BAEKJOON/Not Solved Yet/12865.cpp
+++ BAEKJOON/Not Solved Yet/12865.cpp
@@ -10,11 +10,13 @@ int dp[101][100001] = { 0 };
 
 int main() {
 
-    int total_num, max_weight, temp;
+    int total_num, max_weight, push_weight, temp;
     cin >> total_num >> max_weight;
 
     vector<int> weight;
     vector<int> value;
+    weight.push_back(0);
+    value.push_back(0);
 
     for (int i = 0; i < total_num; i++) {
         cin >> temp;
@@ -23,15 +25,21 @@ int main() {
         value.push_back(temp);
     }
 
-    for (int i = weight[0]; i <= max_weight; i++)
-        dp[1][i] = value[0];
+    for (int i = weight[1]; i <= max_weight; i++) {
+        dp[1][i] = value[1];
+        cout << "i " << 1 << " j " << i << " dp " << dp[1][i] << endl;
+    }
 
     // 다이나믹 프로그래밍
     // dp[i][j] = max(dp[i-1][j], dp[i][j - 넣을 무게] + 넣을 물품의 가치)
-    for (int i = 1; i <= total_num; i++) {
+    for (int i = 2; i <= total_num; i++) {
         for (int j = 1; j <= max_weight; j++) {
-            
+            push_weight = weight[i];
+            if (push_weight - j > 0)
+                dp[i][j] = dp[i - 1][j] > dp[i][j - push_weight] + value[i]? dp[i - 1][j] : dp[i][j - push_weight] + value[i];
+            cout << "i " << i << " j " << j << " dp " << dp[i][j] << endl;
         }
     }
 
-}
+    cout << dp[total_num][max_weight];
+}   
