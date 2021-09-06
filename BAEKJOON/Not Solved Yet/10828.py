1fddee284ac1f08350e2264f42f05eda5be98928
superbottlegod <superbottlegod@naver.com>
2021-09-06 20:05:09 +0900

    9/6

--- BAEKJOON/Not Solved Yet/10828.py
+++ BAEKJOON/Not Solved Yet/10828.py
@@ -2,22 +2,53 @@
 # 기본 스택 자료구조 파일로써 완성하면
 # 복사해서 파일하나 빼놓기
 
+class Node():
+    def __init__(self, data):
+        self.next = None
+        self.data = data
+
+
 class Stack():
 
     def __init__(self):
-        pass
+        self.top_node = None
+        self.first_node = None
+        self.element = 0
 
-    def push(item):
-        pass
+    def push(self, item):
+        new_node = Node(item)
+        if self.top_node == None:
+            self.top_node = new_node
+            self.first_node = new_node
+        else:
+            self.top_node.next = new_node
+            self.top_node = new_node
+        self.element += 1
 
-    def pop():
-        pass
+    def pop(self):
+        if self.top_node == None:
+            return -1
+        else:
+            temp = self.first_node
+            pop_data = self.top_node.data
+            for i in range(self.element - 1):
+                temp = self.first_node.next
+                self.top_node = temp
+                self.top_node.next = None
+            return pop_data
 
-    def size():
-        pass
+    def size(self):
+        return self.element
 
-    def empty():
-        pass
+    def empty(self):
+        if self.element == 0:
+            return 1
+        else:
+            return 0
 
-    def top():
-        pass
+    def top(self):
+        if self.top_node == None:
+            return -1
+        else:
+            return self.top_node.data
+
