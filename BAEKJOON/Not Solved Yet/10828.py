e73649733fad1c58cf9c329d51addda538049bf2
superbottlegod <superbottlegod@naver.com>
2021-09-07 21:51:05 +0900

    9/7

--- BAEKJOON/Not Solved Yet/10828.py
+++ BAEKJOON/Not Solved Yet/10828.py
@@ -1,10 +1,11 @@
 # 10828 - 스택 
 # 기본 스택 자료구조 파일로써 완성하면
 # 복사해서 파일하나 빼놓기
+# pop하고 push가 안됌
 
 class Node():
     def __init__(self, data):
-        self.next = None
+        self.next_node = None
         self.data = data
 
 
@@ -21,7 +22,7 @@ class Stack():
             self.top_node = new_node
             self.first_node = new_node
         else:
-            self.top_node.next = new_node
+            self.top_node.next_node = new_node
             self.top_node = new_node
         self.element += 1
 
@@ -29,13 +30,14 @@ class Stack():
         if self.top_node == None:
             return -1
         else:
-            temp = self.first_node
             pop_data = self.top_node.data
-            for i in range(self.element - 1):
-                temp = self.first_node.next
-                self.top_node = temp
-                self.top_node.next = None
-            return pop_data
+            temp = self.first_node
+            while temp.next_node != self.top_node:
+                temp = temp.next_node
+            self.top_node = temp
+            temp.next_node = None
+        self.element -= 1
+        return pop_data
 
     def size(self):
         return self.element
@@ -52,3 +54,17 @@ class Stack():
         else:
             return self.top_node.data
 
+num = int(input())
+stack = Stack()
+for i in range (num):
+    input_split = input().split()
+    if input_split[0] == "push":
+        stack.push(input_split[1])
+    elif input_split[0] == "pop":
+        print(stack.pop())
+    elif input_split[0] == "size":
+        print(stack.size())
+    elif input_split[0] == "empty":
+        print(stack.empty())
+    elif input_split[0] == "top":
+        print(stack.top())
