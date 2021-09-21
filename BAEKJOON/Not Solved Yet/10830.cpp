160e218fa42ff451ee28a813e99423612fd6c946
superbottlegod <superbottlegod@naver.com>
2021-09-21 23:03:03 +0900

    9/21

--- BAEKJOON/Not Solved Yet/10830.cpp
+++ BAEKJOON/Not Solved Yet/10830.cpp
@@ -4,15 +4,24 @@
 
 using namespace std;
 
-int arr[5][5] = {0};
+int arr[5][5] = { 0 };
+
+void matrix_multiple(int a[][5], int b[][5]);
 
 int main() {
     int n, size;
     cin >> n >> size;
 
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++){
-            arr[i][j] = cin.get();
-        }
+        for (int j = 0; j < n; j++)
+            cin >> arr[i][j];
     }
+
+    while (n > 0) {
+
+    }
+}
+
+void matrix_multiple(int a[][5], int b[][5]) {
+
 }
