7fbdf46240ddeb71a075a32ce084153be6362d84
superbottlegod <superbottlegod@naver.com>
2021-10-09 22:44:02 +0900

    10/9

--- BAEKJOON/Not Solved Yet/1644.cpp
+++ BAEKJOON/Not Solved Yet/1644.cpp
@@ -1,10 +1,18 @@
 // 1644 - 소수의 연속합
+// 에라토스테네스의 체 + 투 포인터
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
+    
+    int n;
+    int result = 0;
+    vector<int> pn;
+    cin >> n;
 
+    for (int i = 2; i <= sqrt(n); i++)
     return 0;
 }
