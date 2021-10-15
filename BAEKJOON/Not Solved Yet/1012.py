2d5a06aa08367c54e8fd63268c9c97c8e58fe9c3
superbottlegod <superbottlegod@naver.com>
2021-10-15 15:35:02 +0900

    10/15

--- BAEKJOON/Not Solved Yet/1012.py
+++ BAEKJOON/Not Solved Yet/1012.py
@@ -1,2 +1,18 @@
-# 유기농 배추
+# 1012 - 유기농 배추
+
+case = int(input())
+
+M, N, K = map(int, input().split())
+arr = [[0 for _ in range(M)] for _ in range(N)]
+visit = [[False for _ in range(M)] for _ in range(N)]
+count = 0
+for _ in range(K):
+    i, j = map(int, input().split())
+    arr[i][j] = 1
+
+for i in range(N):
+    for j in range(M):
+        if arr[i][j] == 1 and visit[i][j] == False:
+            # bfs
+            pass
 
---
+++ BAEKJOON/Not Solved Yet/5639.cpp
@@ -0,0 +1,12 @@
+// 5639 - 이진 검색 트리
+// 재귀적으로 호출
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    return 0;
+}
