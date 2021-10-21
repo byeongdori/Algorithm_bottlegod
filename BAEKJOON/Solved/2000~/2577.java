fe2fd2ac59f734f63e90f099e08635ecc6f2d8a8
superbottlegod <superbottlegod@naver.com>
2021-10-21 16:12:28 +0900

    10/21

---
+++ BAEKJOON/Solved/2000~/2577.java
@@ -0,0 +1,24 @@
+// 2577 - 숫자의 개수
+
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) {
+        
+//         Scanner input = new Scanner(System.in);
+
+//         int value = (input.nextInt() * input.nextInt() * input.nextInt());
+//         String valuestr = Integer.toString(value);
+//         input.close();
+
+//         int[] arr = new int[10];
+
+//         for (int i = 0; i < valuestr.length(); i++) {
+//             arr[(valuestr.charAt(i) - 48)]++;
+//         }
+
+//         for (int v : arr) {
+//             System.out.println(v);
+//         }
+//     }
+// }
