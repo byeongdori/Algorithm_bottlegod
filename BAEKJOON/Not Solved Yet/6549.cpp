1e986a191fe1cdb42b2c52579d2619e0aa001ce4
superbottlegod <superbottlegod@naver.com>
2021-11-13 23:59:52 +0900

    11/13

--- BAEKJOON/Not Solved Yet/6549.cpp
+++ BAEKJOON/Not Solved Yet/6549.cpp
@@ -1,10 +1,27 @@
 // 6549 - 히스토그램에서 가장 큰 직사각형
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
 
+    while (1) {
+        int num;
+        vector<int> histogram;
+        cin >> num;
+        if (num == 0)
+            return 0;
+        for (int i = 0; i < num; i++) {
+            int temp;
+            cin >> temp;
+            histogram.push_back(temp);
+        }
+        
+    }
+
+    vector<int> a; 
+    int test_case;
     return 0;
 }
