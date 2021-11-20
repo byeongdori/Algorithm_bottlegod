0dd393b47600b1cf01c00abf73ad0163c4fae377
superbottlegod <superbottlegod@naver.com>
2021-11-20 11:27:43 +0900

    11/20

--- BAEKJOON/Not Solved Yet/6549.cpp
+++ BAEKJOON/Not Solved Yet/6549.cpp
@@ -6,11 +6,19 @@
 
 using namespace std;
 
+int seg_tree[100001];
+vector<int> histogram;
+
+int find_min_index(int left, int right);
+
 int main() {
 
     while (1) {
-        int num;
-        vector<int> histogram;
+
+        // 값 넣는 과정
+        int min_index;
+        long long num;
+        long long min_height = 1000000000;
         cin >> num;
         if (num == 0)
             return 0;
@@ -19,10 +27,29 @@ int main() {
             cin >> temp;
             histogram.push_back(temp);
         }
+        
 
+        // 구간에서 최소 찾기
+        for (int i = 0; i< histogram.size(); i++) {
+            if (min_height > histogram[i]) {
+                min_height = histogram[i];
+                min_index = min_height;
+            }
+        }
     }
 
     vector<int> a; 
     int test_case;
     return 0;
+}
+
+// 최소 높이 가진 블록 재귀적으로 찾기
+int find_min_index(int left, int right) {
+    
+    if (left == right)
+        return left;
+    
+    
+    int mid = (left + right) / 2;
+
 }
