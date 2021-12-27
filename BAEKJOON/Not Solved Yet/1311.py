b05a9b1bc3dad5e04437b0e31e89f99aefed5c3c
superbottlegod <superbottlegod@naver.com>
2021-12-27 20:04:21 +0900

    12/27

--- BAEKJOON/Not Solved Yet/1311.py
+++ BAEKJOON/Not Solved Yet/1311.py
@@ -1,6 +1,6 @@
 # 1311 - 할 일 정하기 1
 # dp, 비트 마스킹
-
+# 이해 다시 해보기
 import sys
 
 N = int(input())
@@ -8,7 +8,16 @@ cost = []
 for _ in range(N):
     cost.append(list(map(int, sys.stdin.readline().split())))
 dp = [sys.maxsize] * (1 << N)
+dp[0] = 0
 
 # dp 진행
+for i in range(1 << N):
+    k = 0
+    for j in range(N):
+        if i & (1 << j):
+            k += 1
+    for j in range(N):
+        if i & (1 << j) == 0 and dp[i|(1 << j)] > dp[i]+cost[k][j]:
+            dp[i|(1 << j)] = dp[i] + cost[k][j]
 
-print(dp)
+print(dp[-1])
